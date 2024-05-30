c617bd89dc0c57781d344955462caffcfbd536aa
Varshakaranam18 <varshakaranam18@gmail.com>
2024-05-30 17:19:57 -0400

    updated admin url

--- backend/controllers/ordereController.js
+++ backend/controllers/ordereController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Place user order for frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://food-2-xmyu.onrender.com";
 
     try {
         console.log("Creating new order...");
